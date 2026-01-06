a2fe73ffc444b8a42094fbd9d5d29d5102f89eea
informaticacomense <g.corti@informaticacomense.it>
2026-01-06 11:59:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.get('/', async (req, res) => {
   try {
     await pool.query('select 1');
     res.send('Backend attivo e DB collegato');
-  } catch (e) {
+  } catch (error) {
+    console.error('ERRORE CONNESSIONE DB:', error.message);
     res.status(500).send('Errore DB');
   }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server avviato');
+  console.log(`Server avviato sulla porta ${PORT}`);
 });

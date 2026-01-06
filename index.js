dac4360fd029cdc8fa1e00a645a4a2c21d4d8378
informaticacomense <g.corti@informaticacomense.it>
2026-01-06 11:37:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import pkg from 'pg';
+
+const { Pool } = pkg;
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    await pool.query('select 1');
+    res.send('Backend attivo e DB collegato');
+  } catch (e) {
+    res.status(500).send('Errore DB');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server avviato');
+});

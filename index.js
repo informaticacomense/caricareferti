59539886cce2e04fbe3acde27317408c5c2b1bed
informaticacomense <g.corti@informaticacomense.it>
2026-01-07 13:08:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const { Pool } = pkg;
    APP
 ========================= */
 const app = express();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// SERVE IL FRONTEND
+app.use('/frontend', express.static(path.join(__dirname, 'frontend')));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
